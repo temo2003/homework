25638df17c36db0e2a84137e3a1dd87e0df6f3f6
temo2003 <temosolomnishvili123@gmail.com>
2023-03-16 23:26:06 +0400

    second commit

--- day2/lesson2.py
+++ day2/lesson2.py
@@ -1,1 +1,78 @@
-print("this is a lesson 2")
+# name = "nika"
+# print(len(name))   #len   lenght
+#            #4     
+  
+
+# my_sentence = """გამარჯობა, შეგახსენებთ რომ სწავლას იწყებს 19 სექტემბერს,
+# გთხოვთ არ გაააცდინოთ"""
+
+# print(my_sentence)
+
+
+
+
+# if 10>5:
+#     print("hello")
+#      #tab-ის საშუალებით მოვახდინეთ ინდენტაცია (indentantion)
+
+# if true:
+#     print("hello") 
+
+
+
+# print(len(full_name))
+
+# #ყველა სტრინგი შეგვიძლია მივიჩნიოდ სიად
+#full_name = "temo solomnishvili"
+# print(full_name[0])
+# #position=index სინონიმებია
+# #ახლა მოვნიშნავ L-ს 
+# print(full_name[7])
+
+#print("z" not in full_name)
+
+
+
+# print(full_name[2:7])
+# print(full_name[2:9:2])
+# #strart:finish:step
+# print(full_name[:5])
+# print(full_name[5:])
+
+full_name = "temo solomnishvili"
+# print(full_name[-1])
+# print(full_name[-7])#isev i ogond bolodan mesame
+# print(full_name[-7] + "abc")
+# print(full_name[-1:-6:-1])
+#სტრინგებს აქვთ მეთოდები 
+#1) სტრინგ. upper()
+#2) len(string)
+# print(full_name.upper())
+name2 = "  temo  "
+# print(name2)
+
+
+print(name2.replace(" ", "<3"))
+
+print(name2.replace("o", "<3"))
+print(full_name.replace("o", "<3"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
