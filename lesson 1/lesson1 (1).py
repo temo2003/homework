d0c39c86329cd5ada247b9e9de506569cd21b08a
temo2003 <temosolomnishvili123@gmail.com>
2023-03-16 20:28:19 +0400

    first commit

---
+++ lesson 1/lesson1 (1).py
@@ -0,0 +1,45 @@
+#name = "temo"
+#name aris cvladi 
+# =aris cvladis mnishvnelobis mimniwebeli simbolo
+# "temo aris cvladistvis mnishvneloba  "
+#surname = "solomnishvili"
+#print(name)
+#print(surname)
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+
+#name = "temo"  #es aaris str (string) tipis cvladi
+#age = 19 #es aris int (integer) mTeli ricXvi
+#height = 184.5 # es aris float tipis cvladi (atwiladi)
+# boolean (bool) tipis cvladi
+#knows_programming = True #true an false 
+#is_ugly = False #snakecase (ubralos weris stili standartulad)
+
+#isUgly = False # javaskriptuli camelcase
+
+# print (name + " " + surname)
+
+# print (name + age )
+
+#print (type(age)) 
+#print (type(name)) 
+#print (type(surname)) 
+#print (type(height)) 
+#print (type(knows_programming)) 
+
+#print (name + " " + str(age))
+
+
+name = "temo"
+surname = "solomnishvili"
+age = 19 
+height = 180.3 
+born_location = "kakheti"
+school = "Goal-oriented academy"
+
+print (name + " " + surname + " " + str(age) + " " + str(height ) + " " + born_location + " " + school) 
+
+
+
+
+
