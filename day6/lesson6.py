3be862ab88822d411af9cf874f8ab7612c1b5b57
temo2003 <temosolomnishvili123@gmail.com>
2023-03-20 23:28:13 +0400

    second commit

---
+++ day6/lesson6.py
@@ -0,0 +1,57 @@
+# x = 0
+# for i in range(5):
+#     for j in range(4):
+#      for j in range (3):
+#         print( str(x) + " temo")   # პასუხი 60
+
+#         x += 1
+
+
+# age_of_user = int(input("enter users age: "))
+# #18
+# #24
+# #2
+# #5
+# #42
+
+# #4 *100 = 400
+# total_price = 0
+# for i in range(5):
+#     age_of_user = int (input("enter users age: "))
+#     if age_of_user>=3:
+#         total_price += 100
+
+# print(total_price)
+
+
+# total_price = 0
+# i = 0
+# while i < 5:
+#     age_of_user = int (input("enter users age: "))
+#     if age_of_user>=3:
+#         total_price += 100
+#     i += 1
+
+# print(total_price)
+# # 
+# a = "5.4"
+
+# print(float(a))
+
+
+my_list = ["xinkali", "mwvadi", "lobiani", "qababi", "khachapuri", "wyali"]
+
+prices = [2, 20, 15, 10, 15, 2]
+
+# weird_list = ["nika","nika2", "nika3" [1,2,3], "niak4", [4,5,6]]
+# print(weird_list[3][1])
+        
+print(len(my_list))
+print(len(prices))
+
+i = 0 
+while i < len(my_list):
+    print(my_list[i] + " ღირს " + str(prices[i]) + " ლარი ") 
+    i += 1
+
+
